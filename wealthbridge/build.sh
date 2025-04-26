8931fbe2594eb642885d7634748974fb5763a0c6
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-04-26 15:07:03 +0100

    Update build.sh
    Save

--- wealthbridge/build.sh
+++ wealthbridge/build.sh
@@ -3,9 +3,13 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py makemigrations bank_app
 
-if ! python manage.py migrate bank_app; then
-    echo "Normal migration failed. Attempting --fake migration..."
-    python manage.py migrate bank_app --fake-initial
+# Only make migrations if there are model changes
+if python manage.py makemigrations --check --dry-run | grep 'Migrations for'; then
+    echo "Model changes detected. Creating migrations..."
+    python manage.py makemigrations
 fi
+
+python manage.py migrate --fake-initial
+
+echo "Deployment complete!"
