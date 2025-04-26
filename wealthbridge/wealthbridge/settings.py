fc951018602785d34743b414011dcb6a5198eb73
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-04-26 09:58:32 +0100

    Update settings.py
    Save

--- wealthbridge/wealthbridge/settings.py
+++ wealthbridge/wealthbridge/settings.py
@@ -67,7 +67,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'bank_app.apps.BankAppConfig',
+    'bank_app',
     'cloudinary',
     'cloudinary_storage',
 ]
