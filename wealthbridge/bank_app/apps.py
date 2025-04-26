29d4e22690f684645c1d876c07de64f5276683cf
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-04-26 12:22:00 +0100

    Update apps.py
    Save

--- wealthbridge/bank_app/apps.py
+++ wealthbridge/bank_app/apps.py
@@ -1,28 +1,23 @@
 from django.apps import AppConfig
 from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.core.management import execute_from_command_line
-from django.db.utils import OperationalError, ProgrammingError
-import sys
+from django.db.models.signals import post_migrate
 
+def create_superuser(sender, **kwargs):
+    User = get_user_model()
+    try:
+        if not User.objects.filter(email=settings.SUPERUSER_EMAIL).exists():
+            User.objects.create_superuser(
+                username=settings.SUPERUSER_USERNAME,
+                email=settings.SUPERUSER_EMAIL,
+                password=settings.SUPERUSER_PASSWORD
+            )
+    except (OperationalError, ProgrammingError):
+        pass
 
 class BankAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bank_app'
 
     def ready(self):
-        # Skip auto superuser creation during collectstatic or non-migration commands
-        if 'collectstatic' in sys.argv or 'makemigrations' in sys.argv:
-            return
-
-        User = get_user_model()
-        try:
-            if not User.objects.filter(email=settings.SUPERUSER_EMAIL).exists():
-                User.objects.create_superuser(
-                    username=settings.SUPERUSER_USERNAME,
-                    email=settings.SUPERUSER_EMAIL,
-                    password=settings.SUPERUSER_PASSWORD
-                )
-        except (OperationalError, ProgrammingError):
-            # Handle initial setup where DB might not yet exist or be ready
-            pass
+        post_migrate.connect(create_superuser, sender=self)
