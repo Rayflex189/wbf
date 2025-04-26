5e7c3c6ef3ca57be7661c170e8e4716aea14fff3
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-04-26 14:47:05 +0100

    Update build.sh
    Save

--- wealthbridge/build.sh
+++ wealthbridge/build.sh
@@ -3,9 +3,9 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py makemigrations
+python manage.py makemigrations bank_app
 
-if ! python manage.py migrate; then
+if ! python manage.py migrate bank_app; then
     echo "Normal migration failed. Attempting --fake migration..."
-    python manage.py migrate --fake
+    python manage.py migrate bank_app --fake-initial
 fi
